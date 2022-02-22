73b927a4b71ec7a9d3be7446fd8ab608548fa82f
ke975 <60978634+ke975@users.noreply.github.com>
2022-02-21 22:16:27 -0600

    creación de pagina lugares

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,13 +17,13 @@ export function Navbar() {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light  container">
-  <Link className="navbar-brand text-white" to="#">Traveling-Safe</Link>
+  <Link className="navbar-brand text-white" to="/">Traveling-Safe</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-  <form class="d-flex">
-      <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-success" type="submit">Search</button>
+  <form className="d-flex">
+      <input className="form-control mr-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success" type="submit">Search</button>
     </form>
 
 
--- src/pages/places.jsx
+++ src/pages/places.jsx
@@ -1,37 +1,52 @@
 import React from 'react';
 import { Contact } from '../components/Contact';
 import {Navbar } from '../components/Navbar'
-import {Footer} from '../components/footer';
+import {Footer} from '../components/footer'
 import {Card} from '../components/Card'
-
-
 
 export function Places(){
     return(
-    <>
+    <div>
 
     <header>
         <Navbar/>
     </header>
 
-   <main>
-     
-<Card/>
-  
-   </main>
+    
+
+    <div className="banneer">
+        <div className='contenido container'>
+      <h2>¿Encuentra tu  Proxima Aventura?
+        
+     </h2>
+
+
+     </div>
+    </div>
+
+        <Card/>
+
+      
+
+
+
+
+
+
    
    
 
-  <section>
-
-  <Contact/> 
+    <section>
+    <Contact/> 
+    </section>
+ 
   
-  </section>
+
 
             <Footer/>
       
 
-    </>
+    </div>
 
     )
 }
